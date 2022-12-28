7db730a40c5fb90f7b56ce496f930a47816d05c7
Sanchit Dang <sanchitd5@live.com>
2022-12-28 14:41:06 +0530

    add proxy to avoid cors issue

--- src/helpers/searchUtils/index.ts
+++ src/helpers/searchUtils/index.ts
@@ -8,37 +8,17 @@ import {
 } from 'helpers/parsers';
 import WebScrapper from 'helpers/Scrapper/scrapper';
 
-
-export const searchPleComputersProducts = async (searchTerm: string) => {
-    const json = {
-        MaximumNumberOfItems: 50,
-        OnlyWebsiteSpotlight: false,
-        ReturnAttributes: true,
-        ReturnCategories: true,
-        ReturnMarketingDescription: true,
-        SearchString: searchTerm,
-    };
-    const response = await axios.create({
-        baseURL: 'https://www.ple.com.au/api/',
-    }).post('getItemGrid', json, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    console.log(response.data.data.Items);
-    return [];
-}
+const proxy = process.env.REACT_APP_PROXY_URL ?? '';
 
 export const searchPcCaseGearProducts = async (searchTerm: string) => {
-
     const response = await axios.create({
-        baseURL: 'https://hpd3dbj2io-3.algolianet.com/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(3.35.1)%3B%20Browser%20(lite)&x-algolia-application-id=HPD3DBJ2IO&x-algolia-api-key=9559cf1a6c7521a30ba0832ec6c38499',
+        baseURL: proxy + 'https://hpd3dbj2io-3.algolianet.com/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(3.35.1)%3B%20Browser%20(lite)&x-algolia-application-id=HPD3DBJ2IO&x-algolia-api-key=9559cf1a6c7521a30ba0832ec6c38499',
     }).post('', `{"requests":[{"indexName":"pccg_products","params":"query=${searchTerm}&maxValuesPerFacet=128&page=0&highlightPreTag=%3Cais-highlight-0000000000%3E&highlightPostTag=%3C%2Fais-highlight-0000000000%3E&clickAnalytics=true&facets=%5B%22manufacturers_name%22%2C%22indicator.filter%22%2C%22products_price%22%2C%22categories.lvl0%22%5D&tagFilters="}]}`);
     return response.data.results[0].hits.map(pcCaseGearSearchProductHTMLNodeToProduct)
 }
 
 export const searchScorptecProducts = async (searchTerm: string) => {
-    let scorptecScrapper = new WebScrapper('https://computers.scorptec.com.au');
+    let scorptecScrapper = new WebScrapper(proxy + 'https://computers.scorptec.com.au');
     const htmlContent = await scorptecScrapper.getHTMLFromPathWithParams(`/search`, {
         w: searchTerm
     }); // get the html content
@@ -50,7 +30,7 @@ export const searchScorptecProducts = async (searchTerm: string) => {
 
 export const searchCentrecomProducts = async (searchTerm: string) => {
     const products = await axios.create({
-        baseURL: 'https://computerparts.centrecom.com.au/api/search',
+        baseURL: proxy + 'https://computerparts.centrecom.com.au/api/search',
     }).get('', {
         params: {
             q: searchTerm,
@@ -62,7 +42,7 @@ export const searchCentrecomProducts = async (searchTerm: string) => {
 }
 
 export const searchMSYProducts = async (searchTerm: string) => {
-    let msyScrapper = new WebScrapper('https://www.msy.com.au');
+    let msyScrapper = new WebScrapper(proxy + 'https://www.msy.com.au');
     const htmlContent = await msyScrapper.getHTMLFromPathWithParams(`/search.php`, {
         cat_id: '',
         keywords: searchTerm
@@ -72,7 +52,7 @@ export const searchMSYProducts = async (searchTerm: string) => {
 }
 
 export const searchComputerAllianceProducts = async (searchTerm: string) => {
-    let computerAllianceScrapper = new WebScrapper('https://www.computeralliance.com.au');
+    let computerAllianceScrapper = new WebScrapper(proxy + 'https://www.computeralliance.com.au');
     const htmlContent = await computerAllianceScrapper.getHTMLFromPathWithParams(`/search`, {
         search: searchTerm
     }); // get the html content
@@ -82,7 +62,6 @@ export const searchComputerAllianceProducts = async (searchTerm: string) => {
 
 
 const integrations = [
-    searchPleComputersProducts,
     searchPcCaseGearProducts,
     searchScorptecProducts,
     searchCentrecomProducts,
